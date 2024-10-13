b16a00b9c4e13856ae560d3486ded696e215c12a
Conor Harkin <Harkin-C36@ulster.ac.uk>
2024-10-13 22:41:04 +0100

    made changes

--- Practical3-StudentLibrary/Program.cs
+++ Practical3-StudentLibrary/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Practical3_StudentLibrary;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student1 = new Student();
+        Student student2 = new Student("Jane Doe", 18);
+
+        student1.Display();
+        student2.Display();
+
+        student1.GetOlder(); student2.GetOlder();
+
+        Console.WriteLine("Student Getting Older");
+        student1.Display();
+        student2.Display();
+
+    }
+}
---
+++ Practical3-StudentLibrary/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3_StudentLibrary
+{
+    internal class Student
+    {
+        private int id;
+        private string name;
+        private int age;
+        private static int studentCount = 0;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public static int StudentCount
+        {
+            get { return studentCount; }
+        }
+
+        public Student()
+        {
+            this.name = "John Doe";
+            this.age = 16;
+            this.id = studentCount++;
+        }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.id = studentCount++;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Student ID:  {id}");
+            Console.WriteLine($"Student Name: {Name}");
+            Console.WriteLine($"Student Age: {Age}");
+        }
+
+        public int GetOlder()
+        {
+            return age++;
+        }
+    }
+}
